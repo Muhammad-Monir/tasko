509ff9aa55bd4aaef3f98b51c554870f5038b00a
rakibuli55 <islamrakibul8383@gmail.com>
2024-04-25 11:16:02 +0600

    select issu solved

--- src/components/Home/Task/TaskList.jsx
+++ src/components/Home/Task/TaskList.jsx
@@ -22,7 +22,7 @@ function TaskList() {
   });
 
   return (
-    <div className="overflow-y-scroll max-h-[55vh] mt-14 pr-2 pb-4 ">
+    <div className="tasklist--wrapper overflow-y-scroll max-h-[55vh] mt-14 pr-2 pb-4 ">
       {/* list wrapper */}
       <div className="grid grid-cols-3 gap-6 ">
         {isLoading && <p>data is loading</p>}
